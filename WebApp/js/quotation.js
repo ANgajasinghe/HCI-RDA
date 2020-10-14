afb5a4733364215be770f1e917958839072ea9eb
dhanuka97 <dhanukamadushan97@gmail.com>
2020-10-14 23:50:38 +0530

    Danuka

--- WebApp/js/quotation.js
+++ WebApp/js/quotation.js
@@ -1,64 +1,69 @@
 // // Ads and Bids Forum
-// var firebaseConfig = {
-//     apiKey: "AIzaSyARcxAC0ra1-NmHb-5WL6KXAomUcFo1Y6A",
-//     authDomain: "quotation-3a3dd.firebaseapp.com",
-//     databaseURL: "https://quotation-3a3dd.firebaseio.com",
-//     projectId: "quotation-3a3dd",
-//     storageBucket: "quotation-3a3dd.appspot.com",
-//     messagingSenderId: "653502751755",
-//     appId: "1:653502751755:web:645e9bd421ec2b51afd30e",
-//     measurementId: "G-6YEKQR4JDX"
-// };
+var firebaseConfig = {
+    apiKey: "AIzaSyAPerNpYsXIgjgks-Q_wKcHEhG-oSfZKpo",
+    authDomain: "rda-hci.firebaseapp.com",
+    databaseURL: "https://rda-hci.firebaseio.com",
+    projectId: "rda-hci",
+    storageBucket: "rda-hci.appspot.com",
+    messagingSenderId: "490811445050",
+    appId: "1:490811445050:web:0fcec5b9a96428bdfe8a03",
+    measurementId: "G-DHTHSJKFZF"
+
+  };
 // // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
 
 // // Reference messages collection
-// var quatationRef = firebase.database().ref('quatations');
+var quatationRef = firebase.database().ref("rda-hci");
 
 // // Listen for form submit
-// document.getElementById("quatationForm").addEventListener("submit", submitquatationsForm);
+ document.getElementById("quatationForm").addEventListener("submit", submitquatationsForm);
 
 // // Submit Form
-// function submitquatationsForm(e) {
-//     e.preventDefault();
+function submitquatationsForm(e) {
+    e.preventDefault();
 
-//     var name = getInputValues('name');
-//     var email = getInputValues('email');
-//     var address = getInputValues('address');
-//     var contactnumber = getInputValues('contactnumber');
-//     var company = getInputValues('company');
-//     var subject = getInputValues('subject');
+     var name = getInputValues('name');
+     var email = getInputValues('email');
+     var address = getInputValues('address');
+     var contactnumber = getInputValues('phonenumber');
+     var company = getInputValues('company');
+     var subject = getInputValues('subject');
+     var Description = getInputValues('Description');
+     console.log(document.getElementById('email'));
 
 //     // Save Message
-//     saveMessage(name, email, address, contactnumber, company, subject);
+     saveMessage(name, email, address, contactnumber, company, subject,Description);
 
 //     // Show alert
-//     document.querySelector('.sent-message').style.display = 'block';
+     document.querySelector('.sent-message').style.display = 'block';
 
 //     // Hide Alert
-//     setTimeout(function () {
-//         document.querySelector('.sent-message').style.display = 'none';
-//     }, 2000);
+     setTimeout(function () {
+         document.querySelector('.sent-message').style.display = 'none';
+     }, 2000);
 
 //     // Clear Form
-//     document.getElementById('quatationForm').reset();
+     document.getElementById('quatationForm').reset();
 
 // }
 
 // // Function to get Form values
-// function getInputValues(id) {
-//     return document.getElementById(id).value;
-// }
+function getInputValues(id) {
+    return document.getElementById(id).value;
+}
 
 // // Save the messages to the Firebase
-// function saveMessage(name, email, address, contactnumber, company, subject) {
-//     var newQuatationRef = quatationRef.push();
-//     newQuatationRef.set({
-//         name: name,
-//         email: email,
-//         address: address,
-//         contactnumber: contactnumber,
-//         company: company,
-//         subject: subject
-//     });
-// }
+function saveMessage(name, email, address, contactnumber, company, subject,Description) {
+     var newQuatationRef = quatationRef.push();
+     newQuatationRef.set({
+         name: name,
+         email: email,
+         address: address,
+         contactnumber: contactnumber,
+         company: company,
+         subject: subject,
+         Description: Description
+     });
+    }}
